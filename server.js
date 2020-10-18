6193514b0753b52e3ecc8f4ad1a96925c3f2c14e
Santripta Sharma <santripta@gmail.com>
2020-10-18 23:52:49 +0530

    whoops

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+app.use(express.static("front"));
+
 var ops = [];
 
 app.get("/", (req, res) => {
